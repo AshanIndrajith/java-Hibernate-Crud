ea807a6f4be367eadef8461ed091e3bba2702c52
Ashan Indrajith <103479897+AshanIndrajith@users.noreply.github.com>
2023-05-03 00:19:13 +0530

    up date insert function

---
+++ java-hibernate/src/main/java/entity/Main.java
@@ -0,0 +1,42 @@
+package entity;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction et = em.getTransaction();
+
+        Scanner myObj = new Scanner(System.in);
+
+
+        try {
+             et.begin();
+            StudentEntity stu = new StudentEntity('1',"koshika","TEC001");
+
+
+
+            em.persist(stu);
+             et.commit();
+
+
+        } finally {
+            if (et.isActive()) {
+                et.rollback();
+            }
+            em.close();
+            emf.close();
+        }
+
+
+    }
+
+}
